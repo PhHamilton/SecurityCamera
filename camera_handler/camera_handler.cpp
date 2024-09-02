58cf209729269bab3878724016cb8a7e79073fb5
Philip Hamilton <philip.h.hamilton@gmail.com>
2024-09-02 17:04:30 +0200

    Fixed MQTT

--- camera_handler/camera_handler.cpp
+++ camera_handler/camera_handler.cpp
@@ -5,16 +5,42 @@
 #include <signal.h>
 #include <unistd.h>
 
-CAMERA_HANDLER::CAMERA_HANDLER(std::string outputFilepath) : cameraConfig("config/config.json"), mqttHandler("config/config.json")
+CAMERA_HANDLER::CAMERA_HANDLER(std::string outputFilepath)
+    : _outputFilepath(outputFilepath), _cycle(0), _surveillenceMode(DISABLED),
+      cameraConfig("config/config.json"), MQTT_HANDLER("config/config.json")
+
 {
-    _outputFilepath = outputFilepath;
-    mqttHandler.Initialize();
 }
 
 bool CAMERA_HANDLER::Initialize()
 {
-    mqttHandler.Start();
-    return true;
+    MQTT_HANDLER::Initialize();
+    MQTT_HANDLER::Start();
+}
+
+void CAMERA_HANDLER::_MQTTThread()
+{
+
+}
+
+void CAMERA_HANDLER::message_arrived(mqtt::const_message_ptr msg)
+{
+    std::cout << "Handling message for topic: " << msg->get_topic() << " with payload: " << msg->to_string() << std::endl;
+}
+
+void CAMERA_HANDLER::connected(const std::string& cause)
+{
+    std::cout << "Connected: " << cause << std::endl;
+}
+
+void CAMERA_HANDLER::connection_lost(const std::string& cause)
+{
+    std::cout << "Connection lost: " << cause << std::endl;
+}
+
+void CAMERA_HANDLER::delivery_complete(mqtt::delivery_token_ptr token)
+{
+    std::cout << "Delivery complete for token: " << (token ? token->get_message_id() : -1) << std::endl;
 }
 
 void CAMERA_HANDLER::TakePicture()
--- camera_handler/camera_handler.h
+++ camera_handler/camera_handler.h
@@ -11,63 +11,7 @@ typedef enum
     SURVEILLENCE
 }SURVEILLENCE_MODES;
 
-class CustomMQTTHandler : public MQTT_HANDLER
-{
-    public:
-        CustomMQTTHandler(const std::string& configFilename) : MQTT_HANDLER(configFilename) {}
-    protected:
-    void _MQTTThread() override
-    {
-//        std::cerr << "Starting MQTT thread.." << std::endl;
-        mqtt::const_message_ptr msg;
-        _client->start_consuming();
-        while (_isRunning)
-        {
-            try
-            {
-                if (!_client->is_connected())
-                {
-                    std::cerr << "Lost connection to the MQTT server. Reconnecting..." << std::endl;
-                    _client->reconnect()->wait();
-                    std::cerr << "Reconnected!" << std::endl;
-                }
-
-                // Decompose Message!
-
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-            }
-            catch (const mqtt::exception& exc)
-            {
-                std::cerr << "Error: " << exc.what() << std::endl;
-            }
-        }
-    }
-
-    void connected(const std::string& cause) override
-    {
-        std::cout << "Camera handler connected to the MQTT-server" << std::endl;
-    }
-
-    void connection_lost(const std::string& cause) override
-    {
-        std::cout << "Camera handler lost connection to the MQTT-server" << std::endl;
-    }
-
-    void message_arrived(mqtt::const_message_ptr msg) override
-    {
-        std::cout << "Message arrived:\n"
-              << "Topic:" << msg->get_topic() << "\n"
-              << "Payload:" << msg->to_string() << std::endl;
-    }
-
-    void delivery_complete(mqtt::delivery_token_ptr token) override
-    {
-
-    }
-    //std::cerr << "MQTT thread stopped.." << std::endl;
-};
-
-class CAMERA_HANDLER
+class CAMERA_HANDLER : public MQTT_HANDLER
 {
     public:
         CAMERA_HANDLER(std::string outputFilepath);
@@ -79,7 +23,11 @@ class CAMERA_HANDLER
 
         SURVEILLENCE_MODES _surveillenceMode = DISABLED;
         CONFIG_HANDLER cameraConfig;
-        CustomMQTTHandler mqttHandler;
+        void _MQTTThread() override;
+        void message_arrived(mqtt::const_message_ptr msg) override;
+        void connected(const std::string& cause) override;
+        void connection_lost(const std::string& cause) override;
+        void delivery_complete(mqtt::delivery_token_ptr token) override;
 };
 
 #endif //CAMERA_HANDLER_H
