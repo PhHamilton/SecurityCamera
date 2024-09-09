df38cf2b1f2997ded13e3fd344b0b38d04e29aed
Philip Hamilton <philip.h.hamilton@gmail.com>
2024-09-09 21:11:59 +0200

    Added stream utilities

--- install_script.sh
+++ install_script.sh
@@ -1,1 +1,16 @@
 #!bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libjasper-dev
+sudo apt-get install libqt5gui5 libqt5webkit5 libqt5test5
+sudo apt-get install libhdf5-dev
+
+sudo apt-get install python3-pip
+sudo mv /usr/lib/python3.11/EXTERNALLY-MANAGED /usr/lib/python3.11/EXTERNALLY-MANAGED.old
+sudo pip3 install flask
+sudo pip3 install numpy
+sudo pip3 install opencv-contrib-python
+sudo pip3 install imutils
+sudo pip3 install opencv-python
