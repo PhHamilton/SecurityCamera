d25603f424224dd98c853f3751b879f2191c4f3a
Philip Hamilton <philip.h.hamilton@gmail.com>
2024-10-17 21:35:08 +0200

    Changed name from udp_handler to socket_handler

--- udp_handler/mqtt_handler/mqtt_handler.d
+++
@@ -1,4 +0,0 @@
-obj/../mqtt_handler/mqtt_handler.o: ../mqtt_handler/mqtt_handler.cpp \
- ../mqtt_handler/mqtt_handler.h ../config_handler/config_handler.h
-../mqtt_handler/mqtt_handler.h:
-../config_handler/config_handler.h:
